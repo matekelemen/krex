14cc3bc45db85f35671a7f35cb5559cab5786380
matekelemen <kelemen.mate.12@gmail.com>
2022-10-19 10:31:51 +0200

    move init script

--- applications/WRApplication/WRApplication.py
+++
@@ -1,7 +0,0 @@
-from KratosMultiphysics import _ImportApplication
-from WRApplication import *
-
-application = WRApplication()
-application_name = "WRApplication"
-
-_ImportApplication(application, application_name)
