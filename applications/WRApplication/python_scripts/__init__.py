0bae30ee5caeb5522f07c70694a0f7c89c0f9e59
matekelemen <kelemen.mate.12@gmail.com>
2022-10-27 10:48:17 +0200

    update init

--- applications/WRApplication/python_scripts/__init__.py
+++ applications/WRApplication/python_scripts/__init__.py
@@ -1,1 +1,7 @@
-/home/mate/git/krex/applications/WRApplication/WRApplication.py
+from KratosMultiphysics import _ImportApplication
+from WRApplication import *
+
+application = WRApplication()
+application_name = "WRApplication"
+
+_ImportApplication(application, application_name)
