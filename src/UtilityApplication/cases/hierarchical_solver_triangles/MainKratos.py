26266452e195d75ab266f3828cdc26313a5e88ba
matekelemen <kelemen.mate.12@gmail.com>
2024-01-17 14:19:40 +0100

    add case for 2 connected triangles with hierarchical solver

---
+++ src/UtilityApplication/cases/hierarchical_solver_triangles/MainKratos.py
@@ -0,0 +1,67 @@
+# --- Kratos Imports ---
+import KratosMultiphysics
+import KratosMultiphysics.LinearSolversApplication
+import KratosMultiphysics.StructuralMechanicsApplication
+
+# --- STD Imports ---
+import sys
+import time
+import importlib
+import argparse
+import json
+
+def CreateAnalysisStageWithFlushInstance(cls, global_model, parameters):
+    class AnalysisStageWithFlush(cls):
+
+        def __init__(self, model,project_parameters, flush_frequency=10.0):
+            super().__init__(model,project_parameters)
+            self.flush_frequency = flush_frequency
+            self.last_flush = time.time()
+            sys.stdout.flush()
+
+        def Initialize(self):
+            super().Initialize()
+            sys.stdout.flush()
+
+        def FinalizeSolutionStep(self):
+            super().FinalizeSolutionStep()
+
+            if self.parallel_type == "OpenMP":
+                now = time.time()
+                if now - self.last_flush > self.flush_frequency:
+                    sys.stdout.flush()
+                    self.last_flush = now
+
+    return AnalysisStageWithFlush(global_model, parameters)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("kratos")
+    parser.add_argument("-m",
+                        "--mesh",
+                        dest = "mesh",
+                        type = str,
+                        default = "quadratic")
+    parser.add_argument("-s",
+                        "--solver",
+                        dest = "solver",
+                        type = str,
+                        default = "hierarchical_solver")
+    arguments = parser.parse_args()
+
+    with open("ProjectParameters.json", 'r') as parameter_file:
+        project_json = json.load(parameter_file)
+    project_json["solver_settings"]["model_import_settings"]["input_filename"] = "meshes/" + arguments.mesh
+    project_json["solver_settings"]["linear_solver_settings"]["@include_json"] = "solvers/" + arguments.solver + ".json"
+
+    parameters = KratosMultiphysics.Parameters(json.dumps(project_json))
+
+    analysis_stage_module_name = parameters["analysis_stage"].GetString()
+    analysis_stage_class_name = analysis_stage_module_name.split('.')[-1]
+    analysis_stage_class_name = ''.join(x.title() for x in analysis_stage_class_name.split('_'))
+
+    analysis_stage_module = importlib.import_module(analysis_stage_module_name)
+    analysis_stage_class = getattr(analysis_stage_module, analysis_stage_class_name)
+
+    global_model = KratosMultiphysics.Model()
+    simulation = CreateAnalysisStageWithFlushInstance(analysis_stage_class, global_model, parameters)
+    simulation.Run()
