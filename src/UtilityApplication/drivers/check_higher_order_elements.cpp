a784afa3394b5b6d55cc6f36149db54082921376
matekelemen <kelemen.mate.12@gmail.com>
2024-05-08 14:17:37 +0200

    write out-of-line nodes to file

--- src/UtilityApplication/drivers/check_higher_order_elements.cpp
+++ src/UtilityApplication/drivers/check_higher_order_elements.cpp
@@ -7,8 +7,6 @@
 #include "UtilityApp/ModelPartIO.hpp"
 #include "UtilityApp/common.hpp"
 
-// --- Core Includes ---
-
 // --- Structural Mechanics Includes ---
 #include "structural_mechanics_application.h"
 #include "utilities/geometry_utilities.h" // GeometryUtils::GetGeometryName
@@ -17,6 +15,8 @@
 #include <iostream> // cerr
 #include <filesystem> // path, exists, is_directory
 #include <vector> // vector
+#include <optional> // optional
+#include <fstream> // ofstream
 
 
 namespace Kratos::UtilityApp {
@@ -26,7 +26,7 @@ int main(int argc, const char** argv)
 {
     constexpr double tolerance = 1e-10;
 
-    if (argc != 2) {
+    if (argc < 2) {
         std::cerr << "missing argument for input model part\n";
         return 1;
     }
@@ -37,11 +37,16 @@ int main(int argc, const char** argv)
         return 1;
     }
 
+    std::optional<std::filesystem::path> target_path;
+    if (2 < argc) {
+        target_path.emplace(argv[2]);
+    }
+
     // Load kratos applications
     std::vector<std::unique_ptr<KratosApplication>> applications;
     applications.emplace_back(new KratosApplication("KratosCore"));
     applications.emplace_back(new KratosStructuralMechanicsApplication);
-    for (auto& rp_application : applications) rp_application->Register();
+    for (std::unique_ptr<KratosApplication>& rp_application : applications) rp_application->Register();
 
     // Load the model part
     const auto p_source_io = UtilityApp::IOFactory(source_path);
@@ -54,6 +59,9 @@ int main(int argc, const char** argv)
         return 1;
     }
 
+    std::ofstream log_file("check_higher_order_elements.csv", std::ios::out);
+    log_file << "NodeID,ElementID,Distance,EdgeLength\n";
+
     // Loop through elements and check node positions
     for (const Element& r_element : r_root.Elements()) {
         Ref<const Geometry<Node>> r_geometry = r_element.GetGeometry();
@@ -62,6 +70,26 @@ int main(int argc, const char** argv)
         switch (r_geometry.GetGeometryType()) {
             case GeoType::Kratos_Point2D: break;
             case GeoType::Kratos_Point3D: break;
+            case GeoType::Kratos_Triangle2D3: break;
+            case GeoType::Kratos_Triangle3D3: break;
+            case GeoType::Kratos_Triangle2D6:
+            case GeoType::Kratos_Triangle3D6: {
+                for (auto [i_left, i_mid, i_right] : std::vector<std::array<int,3>> {{0, 3, 1},
+                                                                                     {1, 4, 2},
+                                                                                     {2, 5, 0}}) {
+                    const array_1d<double,3> midpoint = 0.5 * (r_geometry[i_left] + r_geometry[i_right]);
+                    const array_1d<double,3> diff = midpoint - (const array_1d<double,3>&) r_geometry[i_mid];
+                    const double edge_length = std::sqrt(inner_prod(r_geometry[i_left] - r_geometry[i_right], r_geometry[i_left] - r_geometry[i_right]));
+                    const double distance = std::sqrt(inner_prod(diff, diff));
+                    KRATOS_WARNING_IF(GeometryUtils::GetGeometryName(r_geometry.GetGeometryType()) + ":", tolerance < distance / edge_length)
+                        << "high order node " << r_geometry[i_mid].Id() << " in element " << r_element.Id()
+                        << " is off by " << distance << " on edge with length " << edge_length
+                        << " (error relative to edge length: " << (edge_length ? distance / edge_length : distance) << "\n";
+                    (array_1d<double,3>&) r_geometry[i_mid] = midpoint;
+                    log_file << r_geometry[i_mid].Id() << ',' << r_element.Id() << ',' << distance << ',' << edge_length << '\n';
+                }
+                break;
+            }
             case GeoType::Kratos_Tetrahedra3D4: break;
             case GeoType::Kratos_Tetrahedra3D10: {
                 for (auto [i_left, i_mid, i_right] : std::vector<std::array<int,3>> {{0, 4, 1},
@@ -76,13 +104,20 @@ int main(int argc, const char** argv)
                     const double distance = std::sqrt(inner_prod(diff, diff));
                     KRATOS_WARNING_IF(GeometryUtils::GetGeometryName(r_geometry.GetGeometryType()) + ":", tolerance < distance / edge_length)
                         << "high order node " << r_geometry[i_mid].Id() << " in element " << r_element.Id()
-                        << " is off by " << distance << " on edge with length " << edge_length << "\n";
+                        << " is off by " << distance << " on edge with length " << edge_length
+                        << " (error relative to edge length: " << (edge_length ? distance / edge_length : distance) << "\n";
+                    (array_1d<double,3>&) r_geometry[i_mid] = midpoint;
+                    log_file << r_geometry[i_mid].Id() << ',' << r_element.Id() << ',' << distance << ',' << edge_length << '\n';
                 }
                 break;
             } // case Tet3D10N
             default: std::cerr << "Unhandled geometry type: " << GeometryUtils::GetGeometryName(r_geometry.GetGeometryType());
         } // switch element geometry type
     } // for r_element in r_root.Elements()
+
+    if (target_path.has_value()) {
+        UtilityApp::IOFactory(target_path.value())->Write(r_root);
+    }
 
     return 0;
 } // int main
